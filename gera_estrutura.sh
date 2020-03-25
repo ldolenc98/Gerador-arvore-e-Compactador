8639f8a760230e562ee6486d32c2f76543ea3de3
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-03-25 13:50:45 -0300

    Update gera_estrutura.sh

--- gera_estrutura.sh
+++ gera_estrutura.sh
@@ -3,34 +3,34 @@
 if [ ! -d /.caseA-pre-process]
 
 then
-  cd ./
+        cd ./
 
-  mkdir caseA-pre-process
-  mkdir control-files
-  mkdir kafka-tutorial
-  mkdir netezza-extraction
-  mkdir ntz_tdt_to_hive
-  touch ntz_tdt_to_hive.zip
-  mkdir setup-env
-  mkdir spark-netezza
-  mkdir target
-  mkdir use-case-a
+        mkdir caseA-pre-process
+        mkdir control-files
+        mkdir kafka-tutorial
+        mkdir netezza-extraction
+        mkdir ntz_tdt_to_hive
+        touch ntz_tdt_to_hive.zip
+        mkdir setup-env
+        mkdir spark-netezza
+        mkdir target
+        mkdir use-case-a
 
-  cd caseA-pre-process
+        cd caseA-pre-process
 
-  touch backup_submit.sh
-  touch build.sbt
-  mkdir project
-  mkdir spark-warehouse
-  mkdir src
-  touch submit.sh
-  mkdir target
+       touch backup_submit.sh
+       touch build.sbt
+       mkdir project
+       mkdir spark-warehouse
+       mkdir src
+       touch submit.sh
+       mkdir target
 
-  cd ..
+       cd ..
 
-  tree
+       tree
 else 
-  echo "O arquivo já existe"
+        echo "O arquivo já existe"
   
 fi
   
