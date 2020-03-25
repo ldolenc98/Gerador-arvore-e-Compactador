798c820ce19de86d9723208b780a60e62ae033f6
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-03-25 12:04:06 -0300

    Create gera_estrutura.sh

---
+++ gera_estrutura.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if cd ./caseA-pre-process
+
+	echo "Erro"
+  
+else
+	cd ./
+
+	mkdir caseA-pre-process
+	mkdir control-files
+	mkdir kafka-tutorial
+	mkdir netezza-extraction
+	mkdir ntz_tdt_to_hive
+	touch ntz_tdt_to_hive.zip
+	mkdir setup-env
+	mkdir spark-netezza
+	mkdir target
+	mkdir use-case-a
+
+	cd caseA-pre-process
+
+	touch backup_submit.sh
+	touch build.sbt
+	mkdir project
+	mkdir spark-warehouse
+	mkdir src
+	touch submit.sh
+	mkdir target
+
+	cd ..
+
+	tree
+  
+fi
