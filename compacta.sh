3428e50d80ebb1ed978e36756d924963cb6afe03
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-03-26 10:15:01 -0300

    Update compacta.sh

--- compacta.sh
+++ compacta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /projetos_backup
+mkdir /$1_backup
 
 chmod +x gera_estrutura.sh 
 
@@ -9,33 +9,33 @@ chmod +x gera_estrutura.sh
 cd $1
 
 tar -cvf control-files_$(date +%F).tar.gz control-files
-mv control-files_$(date +%F).tar.gz /projetos_backup
+mv control-files_$(date +%F).tar.gz /$1_backup
 tar -cvf kafka-tutorial_$(date +%F).tar.gz kafka-tutorial
-mv kafka-tutorial_$(date +%F).tar.gz /projetos_backup
+mv kafka-tutorial_$(date +%F).tar.gz /$1_backup
 tar -cvf netezza-extraction_$(date +%F).tar.gz netezza-extraction
-mv netezza-extraction_$(date +%F).tar.gz /projetos_backup
+mv netezza-extraction_$(date +%F).tar.gz /$1_backup
 tar -cvf ntz_tdt_to_hive_$(date +%F).tar.gz ntz_tdt_to_hive
-mv ntz_tdt_to_hive_$(date +%F).tar.gz /projetos_backup
+mv ntz_tdt_to_hive_$(date +%F).tar.gz /$1_backup
 tar -cvf ntz_tdt_to_hive.zip_$(date +%F).tar.gz ntz_tdt_to_hive.zip
-mv ntz_tdt_to_hive.zip_$(date +%F).tar.gz /projetos_backup
+mv ntz_tdt_to_hive.zip_$(date +%F).tar.gz /$1_backup
 tar -cvf setup-env_$(date +%F).tar.gz setup-env
-mv setup-env_$(date +%F).tar.gz /projetos_backup
+mv setup-env_$(date +%F).tar.gz /$1_backup
 tar -cvf spark-netezza_$(date +%F).tar.gz spark-netezza
-mv spark-netezza_$(date +%F).tar.gz /projetos_backup
+mv spark-netezza_$(date +%F).tar.gz /$1_backup
 tar -cvf use-case-a_$(date +%F).tar.gz use-case-a
-mv use-case-a_$(date +%F).tar.gz /projetos_backup
+mv use-case-a_$(date +%F).tar.gz /$1_backup
 tar -cvf backup_submit.sh_$(date +%F).tar.gz backup_submit.sh
-mv backup_submit.sh_$(date +%F).tar.gz /projetos_backup
+mv backup_submit.sh_$(date +%F).tar.gz /$1_backup
 tar -cvf build.sbt_$(date +%F).tar.gz build.sbt
-mv build.sbt_$(date +%F).tar.gz /projetos_backup
+mv build.sbt_$(date +%F).tar.gz /$1_backup
 tar -cvf project_$(date +%F).tar.gz project
-mv project_$(date +%F).tar.gz /projetos_backup
+mv project_$(date +%F).tar.gz /$1_backup
 tar -cvf spark-warehouse_$(date +%F).tar.gz spark-warehouse
-mv spark-warehouse_$(date +%F).tar.gz /projetos_backup
+mv spark-warehouse_$(date +%F).tar.gz /$1_backup
 tar -cvf src_$(date +%F).tar.gz src
-mv src_$(date +%F).tar.gz /projetos_backup
+mv src_$(date +%F).tar.gz /$1_backup
 tar -cvf submit.sh_$(date +%F).tar.gz submit.sh
-mv submit.sh_$(date +%F).tar.gz /projetos_backup
+mv submit.sh_$(date +%F).tar.gz /$1_backup
 
 
 
