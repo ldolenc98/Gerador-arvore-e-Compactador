7be4cd0e453c5a2c94eb9d0d1fbf7bf12483ff43
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-03-26 10:36:17 -0300

    Update compacta.sh

--- compacta.sh
+++ compacta.sh
@@ -8,34 +8,12 @@ chmod +x gera_estrutura.sh
 
 cd $1
 
-tar -cvf control-files_$(date +%F).tar.gz control-files
-mv control-files_$(date +%F).tar.gz /$1_backup
-tar -cvf kafka-tutorial_$(date +%F).tar.gz kafka-tutorial
-mv kafka-tutorial_$(date +%F).tar.gz /$1_backup
-tar -cvf netezza-extraction_$(date +%F).tar.gz netezza-extraction
-mv netezza-extraction_$(date +%F).tar.gz /$1_backup
-tar -cvf ntz_tdt_to_hive_$(date +%F).tar.gz ntz_tdt_to_hive
-mv ntz_tdt_to_hive_$(date +%F).tar.gz /$1_backup
-tar -cvf ntz_tdt_to_hive.zip_$(date +%F).tar.gz ntz_tdt_to_hive.zip
-mv ntz_tdt_to_hive.zip_$(date +%F).tar.gz /$1_backup
-tar -cvf setup-env_$(date +%F).tar.gz setup-env
-mv setup-env_$(date +%F).tar.gz /$1_backup
-tar -cvf spark-netezza_$(date +%F).tar.gz spark-netezza
-mv spark-netezza_$(date +%F).tar.gz /$1_backup
-tar -cvf use-case-a_$(date +%F).tar.gz use-case-a
-mv use-case-a_$(date +%F).tar.gz /$1_backup
-tar -cvf backup_submit.sh_$(date +%F).tar.gz backup_submit.sh
-mv backup_submit.sh_$(date +%F).tar.gz /$1_backup
-tar -cvf build.sbt_$(date +%F).tar.gz build.sbt
-mv build.sbt_$(date +%F).tar.gz /$1_backup
-tar -cvf project_$(date +%F).tar.gz project
-mv project_$(date +%F).tar.gz /$1_backup
-tar -cvf spark-warehouse_$(date +%F).tar.gz spark-warehouse
-mv spark-warehouse_$(date +%F).tar.gz /$1_backup
-tar -cvf src_$(date +%F).tar.gz src
-mv src_$(date +%F).tar.gz /$1_backup
-tar -cvf submit.sh_$(date +%F).tar.gz submit.sh
-mv submit.sh_$(date +%F).tar.gz /$1_backup
+tar -cvf $1_$(date +%Y%m%d).tar.gz $1_$(date +%Y%m%d)
+
+mv $1_$(date +%Y%m%d).tar.gz /$1_backup
+
+
+
 
 
 
