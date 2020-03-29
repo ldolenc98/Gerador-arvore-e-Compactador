f59adcce16db7167081d5ca350e3eb37dad5a04d
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-03-29 11:52:29 -0300

    Update compacta.sh

--- compacta.sh
+++ compacta.sh
@@ -2,13 +2,9 @@
 
 mkdir -p /$1_backup
 
-chmod +x gera_estrutura.sh 
-
-./gera_estrutura.sh $1
-
 cd $1
 
-tar -cvf $1_$(date +%Y%m%d).tar.gz $1_$(date +%Y%m%d)
+tar --exclude="$1/target" -cvf $1_$(date +%Y%m%d).tar.gz $1
 
 mv $1_$(date +%Y%m%d).tar.gz /$1_backup
 
